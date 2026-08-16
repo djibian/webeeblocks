3c9399c3c9f007fce770c89e8f3929f9035784c7
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-08-16 18:06:49 +0200

    Use Webots R2025a Robot Window API
    * Use Webots R2025a Robot Window API
    
    * Validate modern Robot Window JavaScript in CI
    
    * Initialize RobotWindow using official R2025a module

--- plugins/robot_windows/blockly/main.js
+++ plugins/robot_windows/blockly/main.js
@@ -194,8 +194,9 @@ document.getElementById("projectTitle").addEventListener("keydown", (e) => {
     if(e.key === "Enter") e.preventDefault();
 });
 
-window.onload = function() {
-    window.robotWindow = webots.window("Blockly");
+window.onload = async function() {
+    const module = await import('https://cyberbotics.com/wwi/R2025a/RobotWindow.js');
+    window.robotWindow = new module.default();
     window.robotWindow.receive = receiveMessage;
 }
 
