0df2e3d0deff7c8a5f4e893ddc75a7a3fa58a4c0
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-08-18 23:23:07 +0200

    Route Crazyflie Submit through atomic WWI mission

--- plugins/robot_windows/blockly/main.js
+++ plugins/robot_windows/blockly/main.js
@@ -3,6 +3,7 @@ var closeButton = document.getElementsByClassName("closeButton")[0];
 var saveList = document.getElementById("saveList");
 
 var title = document.getElementById("projectTitle");
+var crazyflieRuntimeState = "WAITING";
 
 function openModal() {
 
@@ -28,48 +29,37 @@ window.onclick = function(e) {
 }
 
 const SocketCommand = {
-    //kind of like an enum I guess
     SEND_CODE: "SEND_CODE",
     SAVE: "SAVE",
     LIST_SAVES: "LIST_SAVES",
     RESTORE_SAVE: "RESTORE_SAVE",
-    RESTORE_LAST: "RESTORE_LAST", //last submitted or saved file
+    RESTORE_LAST: "RESTORE_LAST",
     RESTORE_LAST_NAME: "RESTORE_LAST_NAME",
     SAVE_LAST: "SAVE_LAST",
-   
 };
 
-var currCommand = null; //stores the current command we are in the middle of processing 
+var currCommand = null;
+var ws = null;
 
-if("WebSocket" in window) { //check if websockets are supported
-    
-    var ws = new WebSocket("ws://localhost:8001/test.py");
+if("WebSocket" in window) {
+    ws = new WebSocket("ws://localhost:8001/test.py");
     ws.onopen = function() {
-
         document.getElementById("submit").disabled = false;
         document.getElementById("save").disabled = false;
         document.getElementById("restore").disabled = false;
 
         currCommand = SocketCommand.RESTORE_LAST_NAME;
         ws.send(currCommand);
-
     };
     ws.onmessage = function (evt) {
-
         var msg = evt.data;
-        
         switch(currCommand) {
-
             case SocketCommand.LIST_SAVES:
-          
                 var files = msg.split("\n");
-
                 saveList.innerHTML = "";
                 modal.style.display = "block";
                 for(i = 0; i< files.length; i++) {
-                    
                     var link = document.createElement("a");
-
                     link.title = files[i];
                     link.onclick = restore;
                     link.style.display = "block";
@@ -80,71 +70,83 @@ if("WebSocket" in window) { //check if websockets are supported
 
                     var css = 'a:hover{ text-decoration: underline;color: blue }';
                     var style = document.createElement('style');
-
-                    if (style.styleSheet) {
+                    if (style.styleSheet)
                         style.styleSheet.cssText = css;
-                    } else {
+                    else
                         style.appendChild(document.createTextNode(css));
-                    }
-                   link.appendChild(style); 
-
+                    link.appendChild(style);
                     saveList.appendChild(link);
                 }
                 if(files.length == 1) {
-                    
                     var text = document.createElement("p");
                     text.innerHTML = "<b>No saved projects</b>";
                     saveList.appendChild(text);
                 }
             break;
             case SocketCommand.RESTORE_SAVE:
-           
-                //alert(msg);
                 Blockly.mainWorkspace.clear();
                 closeModal();
-
-                if(msg != "\0") { //If msg isn't empty
+                if(msg != "\0") {
                     var xml = Blockly.Xml.textToDom(msg);
                     Blockly.Xml.domToWorkspace(xml, Blockly.mainWorkspace);
                 }
             break;
             case SocketCommand.RESTORE_LAST_NAME:
-
                 if(msg != "\0") {
-               
                     title.textContent = msg;
                     ws.send(SocketCommand.RESTORE_LAST);
-                    currCommand = SocketCommand.RESTORE_SAVE; //Restoring last save will use RESTORE_SAVE in the switch statement
+                    currCommand = SocketCommand.RESTORE_SAVE;
                 }
-
             break;
         }
     };
     ws.onclose = function () {
         console.log("Connection Closed");
     }
-
 } else {
-
     alert("WebSocket is not supported");
 }
 
 function saveLast() {
-
+    if (!ws || ws.readyState !== WebSocket.OPEN)
+        return;
     currCommand = SocketCommand.SAVE_LAST;
     ws.send(currCommand);
-
     ws.send(title.textContent);
 }
 
+function isCrazyflieWorkspace() {
+    var topBlocks = workspace.getTopBlocks(true);
+    return topBlocks.length === 1 && topBlocks[0].type.indexOf('webeeblocks_') === 0;
+}
+
+function submitCrazyflieMission() {
+    if (!window.robotWindow || typeof window.robotWindow.send !== 'function')
+        throw new Error('Crazyflie runtime transport is not ready');
+    if (crazyflieRuntimeState === 'RUNNING')
+        throw new Error('Crazyflie mission is already running');
+    var message = WebeeBlocksCrazyflie.workspaceToMissionMessage(workspace);
+    window.robotWindow.send(message);
+}
+
 function convertCode() {
+    if (isCrazyflieWorkspace()) {
+        try {
+            submitCrazyflieMission();
+        } catch (error) {
+            console.error(error);
+        }
+        return;
+    }
 
+    if (!ws || ws.readyState !== WebSocket.OPEN) {
+        console.error('Historical Blockly sidecar is not connected');
+        return;
+    }
     currCommand = SocketCommand.SEND_CODE;
-
     var code = Blockly.Python.workspaceToCode(workspace);
     ws.send(SocketCommand.SEND_CODE);
     ws.send(code);
-
     saveLast();
 }
 function realTimeUpdate() {
@@ -153,29 +155,34 @@ function realTimeUpdate() {
 }
 
 function saveBlocks() {
-
+    if (!ws || ws.readyState !== WebSocket.OPEN)
+        return;
     currCommand = SocketCommand.SAVE;
-
     var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
     ws.send(SocketCommand.SAVE);
     ws.send(title.textContent+".xml");
     ws.send(Blockly.Xml.domToText(xml));
-    
     saveLast();
 }
 
 function restore() {
-
+    if (!ws || ws.readyState !== WebSocket.OPEN)
+        return;
     currCommand = SocketCommand.RESTORE_SAVE;
     ws.send(currCommand);
-
-   ws.send(this.innerText + ".xml");
-   title.textContent = this.innerText;
+    ws.send(this.innerText + ".xml");
+    title.textContent = this.innerText;
 }
 
 function receiveMessage(value) {
-
     console.log(value);
+    if (typeof value === 'string' && value.indexOf('WEBEEBLOCKS_MISSION_V1 ') === 0) {
+        if (value === 'WEBEEBLOCKS_MISSION_V1 ACK')
+            crazyflieRuntimeState = 'RUNNING';
+        else if (value === 'WEBEEBLOCKS_MISSION_V1 DONE' || value.indexOf('WEBEEBLOCKS_MISSION_V1 ERR ') === 0)
+            crazyflieRuntimeState = 'WAITING';
+    }
+    window.dispatchEvent(new CustomEvent('webeeblocks-wwi', {detail: value}));
 }
 
 function onResize(e) {
@@ -198,7 +205,8 @@ window.onload = async function() {
     const module = await import('https://cyberbotics.com/wwi/R2025a/RobotWindow.js');
     window.robotWindow = new module.default();
     window.robotWindow.receive = receiveMessage;
+    // The Crazyflie runtime path does not require the historical WebSocket sidecar.
+    document.getElementById("submit").disabled = false;
 }
 
 var container = document.getElementById("blocklyContainer");
-
