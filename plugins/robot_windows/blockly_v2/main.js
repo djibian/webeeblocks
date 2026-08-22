1e7046740ce71e31065253fd9236ba1ffa96a94a
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-08-22 22:17:18 +0200

    Use local Blockly 13 media in Runtime v2

--- plugins/robot_windows/blockly_v2/main.js
+++ plugins/robot_windows/blockly_v2/main.js
@@ -122,7 +122,7 @@ window.onload = async function() {
   workspace = Blockly.inject('blocklyDiv', {
     toolbox: buildToolbox(runtimeProfile),
     scrollbars: true,
-    media: '../blockly/google-blockly-31ee4ea/media/'
+    media: 'vendor/media/'
   });
   workspace.addChangeListener(onWorkspaceChange);
   window.addEventListener('resize', function() { Blockly.svgResize(workspace); });
