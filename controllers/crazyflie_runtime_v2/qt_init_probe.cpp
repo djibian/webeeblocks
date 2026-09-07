21be76a58d27a70f7325f74c4e45f8df07a7a8d8
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-09-07 19:17:40 +0200

    Move Firefox Qt diagnostic into authorized CI

--- controllers/crazyflie_runtime_v2/qt_init_probe.cpp
+++ controllers/crazyflie_runtime_v2/qt_init_probe.cpp
@@ -8,10 +8,72 @@
 #include <csignal>
 #include <cstdio>
 #include <cstring>
+#include <execinfo.h>
 #include <memory>
 #include <unistd.h>
 
+namespace {
+
+void write_marker(const char *message) {
+  ::write(STDERR_FILENO, message, std::strlen(message));
+}
+
+void fatal_signal_handler(int signal_number) {
+  switch (signal_number) {
+    case SIGSEGV:
+      write_marker("WEBEEBLOCKS_QT_FATAL signal=SIGSEGV\n");
+      break;
+    case SIGABRT:
+      write_marker("WEBEEBLOCKS_QT_FATAL signal=SIGABRT\n");
+      break;
+    case SIGBUS:
+      write_marker("WEBEEBLOCKS_QT_FATAL signal=SIGBUS\n");
+      break;
+    case SIGILL:
+      write_marker("WEBEEBLOCKS_QT_FATAL signal=SIGILL\n");
+      break;
+    case SIGFPE:
+      write_marker("WEBEEBLOCKS_QT_FATAL signal=SIGFPE\n");
+      break;
+    default:
+      write_marker("WEBEEBLOCKS_QT_FATAL signal=UNKNOWN\n");
+      break;
+  }
+
+  void *frames[64];
+  const int count = ::backtrace(frames, 64);
+  write_marker("WEBEEBLOCKS_QT_FATAL_BACKTRACE_BEGIN\n");
+  ::backtrace_symbols_fd(frames, count, STDERR_FILENO);
+  write_marker("WEBEEBLOCKS_QT_FATAL_BACKTRACE_END\n");
+  _exit(128 + signal_number);
+}
+
+bool install_fatal_handlers() {
+  // Warm libgcc's unwinder before any asynchronous fault path.
+  void *warmup[1];
+  (void)::backtrace(warmup, 1);
+
+  struct sigaction action {};
+  action.sa_handler = fatal_signal_handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = SA_RESETHAND;
+
+  for (const int signal_number : {SIGSEGV, SIGABRT, SIGBUS, SIGILL, SIGFPE}) {
+    if (sigaction(signal_number, &action, nullptr) != 0)
+      return false;
+  }
+  return true;
+}
+
+}  // namespace
+
 extern "C" int wb_qt_init_probe(void) {
+  if (!install_fatal_handlers()) {
+    std::fprintf(stderr, "WEBEEBLOCKS_QT_INIT_DIAGNOSTIC HANDLER_INSTALL_FAILED\n");
+    std::fflush(stderr);
+    return 3;
+  }
+
   int argc = 1;
   std::array<char, 64> program_name{};
   std::array<char *, 2> argv{};
@@ -19,12 +81,8 @@ extern "C" int wb_qt_init_probe(void) {
   argv[0] = program_name.data();
   argv[1] = nullptr;
 
-  std::fprintf(
-    stderr,
-    "WEBEEBLOCKS_QT_INIT_DIAGNOSTIC RENDEZVOUS pid=%ld before=QApplication\n",
-    static_cast<long>(::getpid()));
+  std::fprintf(stderr, "WEBEEBLOCKS_QT_INIT_DIAGNOSTIC PRE_QAPPLICATION\n");
   std::fflush(stderr);
-  ::raise(SIGSTOP);
 
   std::unique_ptr<QApplication> application;
   if (!QCoreApplication::instance())
@@ -39,6 +97,7 @@ extern "C" int wb_qt_init_probe(void) {
   std::fprintf(stderr, "WEBEEBLOCKS_QT_INIT_DIAGNOSTIC QFILEDIALOG_CONSTRUCTED\n");
   std::fflush(stderr);
 
+  // Deliberate terminal marker: SIGTRAP is not handled by the fatal handler.
   ::raise(SIGTRAP);
   return 0;
 }
