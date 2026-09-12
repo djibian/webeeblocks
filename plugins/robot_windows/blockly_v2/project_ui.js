09a7f07a497d5a676f4c1c5ea13d096e4c34de4f
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-09-12 07:39:15 +0200

    Preserve Chrome native project-file mode

--- plugins/robot_windows/blockly_v2/project_ui.js
+++ plugins/robot_windows/blockly_v2/project_ui.js
@@ -100,7 +100,7 @@
   async function createDirectTransport() {
     var browserTransport = WebeeBlocksProjectFiles.createBrowserTransport(window, document);
     if (browserTransport.nativeFileSystemAccess) {
-      browserTransport.mode = 'browser-native';
+      browserTransport.mode = 'native';
       return browserTransport;
     }
 
