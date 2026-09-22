f9e159f54b86b6e9f14a23ef28cdc3374586de95
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-09-22 20:42:34 +0200

    Preserve canonical Runtime source contract (#487)

---
+++ controllers/crazyflie_runtime_v2/runtime_entry.c
@@ -0,0 +1,19 @@
+#ifdef main
+#undef main
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+int webeeblocks_runtime_flight_main(void);
+int webeeblocks_progression_sequence_evaluator_main(void);
+
+int main(int argc, char **argv) {
+  if (argc == 2 && strcmp(argv[1], "sequence-evaluator-v1") == 0)
+    return webeeblocks_progression_sequence_evaluator_main();
+  if (argc != 1) {
+    fprintf(stderr, "WEBEEBLOCKS_RUNTIME_V2 FATAL invalid controllerArgs\n");
+    return 2;
+  }
+  return webeeblocks_runtime_flight_main();
+}
